29a99f48b9b7bcb56ac827da54fa180f17ab57e3
Zain Ali <zainali187483@gmail.com>
2024-07-08 08:20:00 +0500

    a change

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import BMICalculator from "./Components/BMICalculator";
 const App = () => {
   return (
     <>
-      <p className="text-3xl font-bold tracking-wide p-4 pl-10">Zain.</p>
+      <p className="text-3xl logo font-bold tracking-wide p-4 pl-10">Zain.</p>
       <div className="text-center mt-[10%] md:mt-[5%] flex flex-col items-center justify-center border-red-100">
         <h1 className="md:text-6xl font-bold text-4xl">BMI Calculator!</h1>
         <h2 className="md:text-3xl text-2xl font-bold">(OVH Weak 1 Task)</h2>
